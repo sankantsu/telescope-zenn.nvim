5939a880d48404f5c47158a259300ce5c8cde180
Godai Takashina <azarashi.azarashi.a@gmail.com>
2023-12-15 01:33:47 +0900

    ensure cwd is zenn project root

--- lua/telescope_zenn/init.lua
+++ lua/telescope_zenn/init.lua
@@ -1,8 +1,34 @@
+local Path = require("plenary.path")
+local Job = require("plenary.job")
 local pickers = require "telescope.pickers"
 local conf = require("telescope.config").values
 local zenn_finder = require("telescope_zenn.finder")
 
+local ensure_zenn_cli_executable = function ()
+  -- Ensure cwd is node.js project root
+  local file = Path:new("./package.json")
+  print(vim.inspect(file))
+  if not file:exists() then
+    vim.notify("./package.json not found. Please ensure that you are at the zenn directory root.", vim.log.levels.WARN)
+    return false
+  end
+
+  -- Ensure zenn-cli is installed
+  local res, code = Job:new({
+    command = "npx",
+    args = { "zenn", "--help" },
+  }):sync()
+  if code ~= 0 then
+    vim.notify("Zenn cli is not executable. Please ensure that you are at the zenn directory root.", vim.log.levels.WARN)
+    return false
+  end
+  return true
+end
+
 local article_picker = function (opts)
+  if not ensure_zenn_cli_executable() then
+    return
+  end
   opts = opts or {}
   pickers.new(opts, {
     prompt_title = "Zenn articles",
